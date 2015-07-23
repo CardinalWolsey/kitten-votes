88533215d175f61aec7fe2fd7cf92ea9bbb8f626
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-07-23 10:37:34 -0700

    Converted most of it to jQuery

--- js/vote-tracker.js
+++ js/vote-tracker.js
@@ -24,6 +24,7 @@ var cat13 = new Image ('images/13.jpg');
 //declare array of cats
 var catArray = [cat0, cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13];
 
+
 var Tracker = function() {
   this.catArray = catArray;
  }
